69347f29fa4397fa4a7e68fabb9c285c2ce11072
Marcin Kośka <m.koska2@wp.pl>
2021-01-24 17:35:12 +0100

    Updates

---
+++ precache-manifest.e795ed8b3bf740f7ac266e64fe5600c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70095db2858325aa820d33261a706acd",
+    "url": "/movieApp/index.html"
+  },
+  {
+    "revision": "0529e890b5c03f8a881c",
+    "url": "/movieApp/static/js/2.8c973fbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieApp/static/js/2.8c973fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c4d5ee54753baa86879",
+    "url": "/movieApp/static/js/main.6da79209.chunk.js"
+  },
+  {
+    "revision": "9683273862b3a76620a2",
+    "url": "/movieApp/static/js/runtime-main.42d387db.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bca88a6ebc0383f3a41900a808978bef.js"
+  "/movieApp/precache-manifest.e795ed8b3bf740f7ac266e64fe5600c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movieApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
